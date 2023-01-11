65d1d9f237b2892fcf7249057cd7a2979706c11a
Bùi Đình Khang <chocopie.nuce@gmail.com>
2023-01-11 21:44:22 +0700

    update home page
    add information about user

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,16 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  intro = {
-    main: 'Web designer',
-    sub: 'Frontend-developer'
+  jobs = ['Frontend developer', 'Web designer'];
+  myJob: string = '';
+  offset = 0;
+  forwards = true;
+  skip_count = 0;
+  skip_delay = 15;
+  speed = 70;
+  infomation = {
+    name: "Bui Đinh Khang",
+    shortName: "Ka",
+    age: 24,
+    address: "Me Linh District, Hanoi",
+    email: "ka.0709fe@gmail.com",
+    phone: "+84 38 662 4545",
+    onWork: false
   }
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    let i = 0;
+    setInterval(() => {
+      if (this.forwards) {
+        if (this.offset >= this.jobs[i].length) {
+          ++this.skip_count;
+          if (this.skip_count === this.skip_delay) {
+            this.forwards = false;
+            this.skip_count = 0;
+          }
+        }
+      } else {
+        if (this.offset == 0) {
+          this.forwards = true;
+          i++;
+          this.offset = 0;
+          if (i >= this.jobs.length) {
+            i = 0;
+          }
+        }
+      }
+      this.myJob = this.jobs[i].substring(0, this.offset);
+      if (this.skip_count === 0) {
+        if (this.forwards) {
+          this.offset++;
+        }
+        else {
+          this.offset--;
+        }
+      }
+    }, this.speed);
   }
-
 }
